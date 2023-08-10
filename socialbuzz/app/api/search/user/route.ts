a7a70aca9fb1fdaa957d7dd7940b40fba3b1874d
Joel Komieter <komieterj@gmail.com>
2023-08-09 23:22:21 -0400

    added export const dynamic = force-dynamic to api/search/user/route.ts

--- socialbuzz/app/api/search/user/route.ts
+++ socialbuzz/app/api/search/user/route.ts
@@ -2,44 +2,45 @@ import prisma from "@/libs/prismadb";
 import getCurrentUser from "@/libs/serverAuth";
 import { NextRequest, NextResponse } from "next/server";
 
-
 // fetch users that match the search query
 export async function GET(request: NextRequest) {
-    try {
-        // get the username query from the request
-        const currentUser = await getCurrentUser();
+  try {
+    // get the username query from the request
+    const currentUser = await getCurrentUser();
 
-        if (!currentUser) return NextResponse.json({error: 'You are not logged in'})
+    if (!currentUser)
+      return NextResponse.json({ error: "You are not logged in" });
 
-        // const { username } = request.query;
-        const username = "JKomieter"
+    // const { username } = request.query;
+    const username = "JKomieter";
 
-        // get the current users or user
+    // get the current users or user
 
-        const filteredUsers = await prisma.user.findMany({
-            select: {
-                username: true,
-                profileImage: true,
-                id: true,
-                firstName: true,
-                lastName: true,
-                followersIds: true
-            },
-            orderBy: {
-                createdAt: 'desc'
-            },
-            where: {
-                username: {
-                    contains: username as string
-                },
-            },
-        });
+    const filteredUsers = await prisma.user.findMany({
+      select: {
+        username: true,
+        profileImage: true,
+        id: true,
+        firstName: true,
+        lastName: true,
+        followersIds: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+      where: {
+        username: {
+          contains: username as string,
+        },
+      },
+    });
 
-        // Return the user data as the API response
-        return NextResponse.json(filteredUsers || []);
-        
-    } catch (error) {
-        console.log(error);
-        return NextResponse.json([]);
-    }
+    // Return the user data as the API response
+    return NextResponse.json(filteredUsers || []);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json([]);
+  }
 }
+
+export const dynamic = "force-dynamic";
